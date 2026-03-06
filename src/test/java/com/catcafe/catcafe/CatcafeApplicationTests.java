dd1f5168c9cb48dbab0047efd1e9f26a79d9a02d
JanP1 <janp1krc@gmail.com>
2026-03-06 13:13:13 +0100

    Initial commit

---
+++ src/test/java/com/catcafe/catcafe/CatcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catcafe.catcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
